4ec789b36d131b75329b025bb8b95d20d01ee3d5
Jonny Spicer <spicerjonny@gmail.com>
2020-07-30 22:00:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#Boudica
+
+This is the beginnings of a chess engine implemented in C# - it is very, very much a work in progress.
+
+## Todo
+- ~~Initialise standard starting position~~
+- ~~standard pawn move generation~~
+- en passant move generation (in progress, currently bugged)
+- sliding piece move generation
+- knight move generation
+- castling move generation
+- implement Perft
+- output UCI
+- search
+- eval
+- frontend
+
+If you're interested in chess programming, the [chess programming wiki](https://www.chessprogramming.org/Main_Page){:target="_blank"}
+is invaluable. It is my primary source of information for writing this engine, and all the techniques I've implemented
+are described on the wiki.
---
+++ boudica/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace boudica
+{
+    public class Helpers
+    {
+        public static int getNumberOfTrailingZeros(long i)
+        {
+            // used for bitscan, see
+            // https://www.chessprogramming.org/BitScan
+            // and
+            // https://stackoverflow.com/questions/18622130/how-to-count-trailing-zeros-in-a-binary-representation-of-an-integer-number-with
+            // TODO: replace this with something less expensive
+            int[] _lookup =
+            {
+                64,  0,  1, 39,  2, 15, 40, 23, 3, 12, 16, 59, 41, 19, 24, 54, 4, -1, 13, 10, 17, 62, 60, 28, 42, 30, 20, 51, 25, 44,
+                55, 47, 5, 32, -1, 38, 14, 22, 11, 58, 18, 53, 63,  9, 61, 27, 29, 50, 43, 46, 31, 37, 21, 57, 52,  8, 26, 49, 45, 36,
+                56, 7, 48, 35, 6, 34, 33, -1
+            };
+
+            return _lookup[(i & -i) % 67];
+        }
+
+        public static long reverseBits(long l)
+        {
+            long tmp = l;
+            long r = 0L;
+
+            if (tmp < 0)
+                tmp *= -1;
+
+            while (tmp > 0)
+            {
+                r = (r * 10) + (tmp - ((tmp / 10)) * 10);
+                tmp = tmp / 10;
+            }
+
+            return r * (l < 0 ? -1 : 1);
+        }
+    }
+}
